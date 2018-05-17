0cc52bd5e669ee46d3c8c35a5b4fdb450d197059
Konstantin Babiy <kb@247e.com>
2018-05-17 20:18:33 +0300

    forgot one

--- LogCast.Tracing/Properties/AssemblyInfo.cs
+++ LogCast.Tracing/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("LogCast.Tracing")]
 [assembly: AssemblyDescription(".Net tracing TraceListener to send logs through LogCast")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("24-7 Entertainment")]
-[assembly: AssemblyCopyright("Copyright 24-7 Entertainment 2016")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("efd79198-2609-4d29-8cd5-aa56c50544c5")]
 [assembly: InternalsVisibleTo("LogCast.Tracing.Test")]
 
-[assembly: AssemblyVersion("8.2.0")]
-[assembly: AssemblyFileVersion("8.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
